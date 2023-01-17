b23c2de41df20ac198bceb2b98978d6e8065eaec
Musa Aktaş <117383627+MusaAktasMA@users.noreply.github.com>
2023-01-17 14:30:23 +0300

    Stream Deck
    Stream Deck

---
+++ Stream-Deck/app.js
@@ -0,0 +1,29 @@
+const sounds = ["teleport", "fiddlesticks", "ornn", "volibear", "mordekaiser", "zhonya"];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement('button');
+    btn.classList.add("btn");
+
+    btn.innerText = sound;
+
+    btn.addEventListener("click", () => {
+        stopSongs();
+        document.getElementById(sound).play();
+    })
+
+    document.getElementById("buttons").appendChild(btn);
+})
+
+function stopSongs() {
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound);
+
+        song.pause();
+        song.currentTime = 0
+    })
+}
+
+
+
+
+
